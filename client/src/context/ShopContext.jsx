7d965fafbdde681b0a3af7b00ef271943f118cbc
Amit Pattanaik <161040099+amitPattanaik987@users.noreply.github.com>
2024-10-20 19:32:45 +0530

    Update ShopContext.jsx

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -17,7 +17,7 @@ export default function ShopContextProvider(props) {
     const [all_product, setall_product] = useState([]);
     const [cartItems, setcartItems] = useState(getDefaultCart());
     const [count, setcount] = useState(0);
-    const url="https://e-commerce-2-0zxi.onrender.com"
+    const url="https://e-commerce-hat0.onrender.com"
 
     useEffect(() => {
         let sum = 0;
@@ -32,14 +32,14 @@ export default function ShopContextProvider(props) {
 
 
     useEffect(() => {
-        fetch("https://e-commerce-2-0zxi.onrender.com/allproducts")
+        fetch("https://e-commerce-hat0.onrender.com/allproducts")
             .then((res) => res.json())
             .then((data) => {
                 setall_product(data)
             })
 
         if (localStorage.getItem("auth-token")) {
-            fetch("https://e-commerce-2-0zxi.onrender.com/getcart",{
+            fetch("https://e-commerce-hat0.onrender.com/getcart",{
                 method: "POST",
                 headers: {
                     Accept: "application/form-data",
@@ -61,7 +61,7 @@ export default function ShopContextProvider(props) {
 
         setcartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         if (localStorage.getItem("auth-token")) {
-            fetch("https://e-commerce-2-0zxi.onrender.com/addtocart", {
+            fetch("https://e-commerce-hat0.onrender.com/addtocart", {
                 method: "POST",
                 headers: {
                     Accept: "application/formData",
@@ -78,7 +78,7 @@ export default function ShopContextProvider(props) {
         setcartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
 
         if (localStorage.getItem("auth-token")) {
-            fetch("https://e-commerce-2-0zxi.onrender.com/removefromcart", {
+            fetch("https://e-commerce-hat0.onrender.com/removefromcart", {
                 method: "POST",
                 headers: {
                     Accept: "application/formData",
