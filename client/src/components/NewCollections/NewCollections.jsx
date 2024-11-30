9c116d2b79f25fc87a375174d04b15d6cea7ee68
Amit Pattanaik <161040099+amitPattanaik987@users.noreply.github.com>
2024-11-30 22:08:20 +0530

    Update NewCollections.jsx

--- client/src/components/NewCollections/NewCollections.jsx
+++ client/src/components/NewCollections/NewCollections.jsx
@@ -8,7 +8,7 @@ export default function NewCollections() {
   const [new_collection, setnew_collection] = useState([]);
 
   useEffect(() => {
-    fetch("https://e-commerce-hat0.onrender.com/newcollection")
+    fetch("https://e-commerce-2-0zxi.onrender.com/newcollection")
     .then((res)=>res.json())
     .then((data)=>setnew_collection(data));
   }, [])
